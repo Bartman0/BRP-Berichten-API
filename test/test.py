d8bbde411c9f9dc6e9210c07b5573ec30f3df641
Richard Kooijman <kooijman.richard@gmail.com>
2025-03-03 22:32:23 +0100

    testing

--- test/test.py
+++ test/test.py
@@ -1,30 +1,55 @@
-plaats_bericht = """
-{{
-  "$schema" : "berichten.schema.json#/berichtsoorten/Ap01Bericht",
-  "berichtType" : "Ap01",
-  "herhaling" : "0",
-  "plData" : {{
-    "c01" : [ {{
-      "e0120" : "{}"
-    }} ]
-  }}
-}}
-"""
+import os
 
-verwijder_bericht = """
-{{
-  "$schema" : "berichten.schema.json#/berichtsoorten/Av01Bericht",
-  "berichtType" : "Av01",
-  "herhaling" : "0",
-  "plData" : {{
-    "c01" : [ {{
-      "e0120" : "{}"
-    }} ]
-  }}
-}}
-"""
+from client.berichten_api import Configuration, ApiClient
+from client.berichten_api.api.berichtenverkeer_api import BerichtenverkeerApi
+
+ap01_template = {
+    "$schema": "berichten.schema.json#/berichtsoorten/Ap01Bericht",
+    "berichtType": "Ap01",
+    "herhaling": "0",
+    "plData": {"c01": [{"e0120": "000000000"}]},
+}
+
+av01_template = {
+    "$schema": "berichten.schema.json#/berichtsoorten/Av01Bericht",
+    "berichtType": "Av01",
+    "herhaling": "0",
+    "plData": {"c01": [{"e0120": "000000000"}]},
+}
+
+
+put_message_kenmerken_template = {
+    "berichtId": "000",
+    # "verwijzingBerichtId": None,
+    "berichtType": "",
+    "ontvanger": 0,
+}
 
 
 if __name__ == "__main__":
-    print(plaats_bericht.format("123456789"))
-    print(verwijder_bericht.format("123456789"))
+    conf = Configuration(
+        server_index=3,
+        username="10",
+        password=os.environ["BERICHTEN_API_PASSWORD"],
+    )
+    api_client = ApiClient(conf)
+    berichten_client = BerichtenverkeerApi(api_client)
+
+    ap01_bericht_inhoud = ap01_template.copy()
+    ap01_bericht_inhoud["plData"]["c01"][0]["e0120"] = "177022045"
+
+    av01_bericht_inhoud = av01_template.copy()
+    av01_bericht_inhoud["plData"]["c01"][0]["e0120"] = "177022045"
+
+    ap01_put_message_kenmerken = put_message_kenmerken_template.copy()
+    ap01_put_message_kenmerken["berichtId"] = "1"
+    ap01_put_message_kenmerken["berichtType"] = "Ap01"
+    ap01_put_message_kenmerken["ontvanger"] = 2
+
+    ap01_bericht = {
+        "berichtKenmerken": ap01_put_message_kenmerken,
+        "berichtInhoud": ap01_bericht_inhoud,
+    }
+    result = berichten_client.put_messages({"berichten": [ap01_bericht]})
+
+    pass
